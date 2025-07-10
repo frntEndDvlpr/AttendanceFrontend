5905abc1a4068537b6bfe0e9d2dd3d04fff2a5ec
Abbas <reactdvlpr@gmail.com>
2025-07-10 19:59:12 +0400

    Initial project upload

---
+++ app/config/settings.js
@@ -0,0 +1,25 @@
+import Constants from "expo-constants";
+
+const settings = {
+  dev: {
+    apiUrl: "https://attendance-ksa-247f689c4e51.herokuapp.com/",
+  },
+  staging: {
+    apiUrl: "https://attendance-ksa-247f689c4e51.herokuapp.com/",
+  },
+  prod: {
+    apiUrl: "https://attendance-ksa-247f689c4e51.herokuapp.com/",
+  },
+};
+
+const getCurrentSettings = () => {
+  if (__DEV__) return settings.dev;
+
+  const manifest = Constants.expoConfig || Constants.manifest;
+  const releaseChannel = manifest?.releaseChannel;
+
+  if (releaseChannel === "staging") return settings.staging;
+  return settings.prod;
+};
+
+export default getCurrentSettings();
